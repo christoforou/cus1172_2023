f5c3eaf63b4735a847d46f58774af5969d4d273a
christoforou <42910823+christoforou@users.noreply.github.com>
2024-03-03 11:43:59 -0500

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,17 @@
+ document.addEventListener('DOMContentLoaded', ()=>{
+            console.log('The button is pressed XXX');
+            console.log(window)
+            console.log(window.parent)  
+            console.log(window.parent.parent.window.config);
+            //console.log(window.parent.window.config);
+            
+            console.log('The button is pressed');
+            console.log(window);
+            console.log(document.baseURI);
+            console.log(document.parent);
+            console.log(window.parent.window.config);
+            console.log(window.parent.window.document);
+            console.log('The button is pressed END');
+            window.document.querySelector('#myFrameMain').src='https://preactjs.com/tutorial/01-vdom/';
+            window.parent.window.document.querySelector("#app").style.backgroundColor = 'blue';        
+});
