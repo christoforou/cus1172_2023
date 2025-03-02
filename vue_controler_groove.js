b740ac65d25f6d433c27c31ee7add3825dacece8
christoforou <42910823+christoforou@users.noreply.github.com>
2025-03-02 12:48:07 -0500

    Update vue_controler_groove.js
    Added AddContentLoaded.

--- vue_controler_groove.js
+++ vue_controler_groove.js
@@ -1,15 +1,17 @@
-alert("Installing DOM observer");
-
-// Your custom function
-function myCustomHandler(event) {
-  console.log('$nextTick was called!');
-  console.log(window.app.__vue__.$route)
-  console.log(event);
-}
-const originalNextTick  = window.app.__vue__.__proto__.$nextTick;
-
-window.app.__vue__.__proto__.$nextTick = (callback) => {
-    myCustomHandler(this);
-    originalNextTick.call(this, callback); 
-    return 
+document.addEventListener('DOMContentLoaded', function() {
+  alert("Installing DOM observer");
+  
+  // Your custom function
+  function myCustomHandler(event) {
+    console.log('$nextTick was called!');
+    console.log(window.app.__vue__.$route)
+    console.log(event);
+  }
+  const originalNextTick  = window.app.__vue__.__proto__.$nextTick;
+  
+  window.app.__vue__.__proto__.$nextTick = (callback) => {
+      myCustomHandler(this);
+      originalNextTick.call(this, callback); 
+      return 
+  }
 }
