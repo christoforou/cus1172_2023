e0a3aa2014cf734b9b7e47c4439ae5537256f33e
christoforou <42910823+christoforou@users.noreply.github.com>
2025-03-02 12:44:11 -0500

    Update vue_controler_groove.js
    Commit Changes

--- vue_controler_groove.js
+++ vue_controler_groove.js
@@ -1,1 +1,15 @@
-alert("Installing DOM observer") 
+alert("Installing DOM observer");
+
+// Your custom function
+function myCustomHandler(event) {
+  console.log('$nextTick was called!');
+  console.log(window.app.__vue__.$route)
+  console.log(event);
+}
+const originalNextTick  = window.app.__vue__.__proto__.$nextTick;
+
+window.app.__vue__.__proto__.$nextTick = (callback) => {
+    myCustomHandler(this);
+    originalNextTick.call(this, callback); 
+    return 
+}
