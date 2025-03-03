994f36756c4aaf9b5a3fde9435e4b07d0a7d4ff1
christoforou <42910823+christoforou@users.noreply.github.com>
2025-03-03 14:42:42 -0500

    Update customize_course_view.js
    Updated the css to hide left sidebar. and commented the myFrameMain temporarily.

--- customize_course_view.js
+++ customize_course_view.js
@@ -15,10 +15,13 @@ document.addEventListener('DOMContentLoaded', function() {
     console.log(window.parent.window.config);
     console.log(window.parent.window.document);
     console.log('The button is pressed END');
-    window.document.querySelector('#myFrameMain').src='https://preactjs.com/tutorial/01-vdom/';
-    window.parent.window.document.querySelector("#app").style.backgroundColor = 'blue';     
+    // To activate later
+    //window.document.querySelector('#myFrameMain').src='https://preactjs.com/tutorial/01-vdom/';
+    //window.parent.window.document.querySelector("#app").style.backgroundColor = 'blue';     
     //
-  
+
+    // Hide the left sidebar and make the right main content full screen. 
     window.parent.window.document.querySelector(".post-canvas-container").className = 'w-full md:w-full post-canvas-container';
-    window.parent.window.document.querySelector(".post-canvas-container").parentElement.firstChild.className = 'md:w-1/12 w-full flex flex-col space-y-5 md:mt-0 mt-5'
+    //window.parent.window.document.querySelector(".post-canvas-container").parentElement.firstChild.className = 'md:w-1/12 w-full flex flex-col space-y-5 md:mt-0 mt-5'
+    window.parent.window.document.querySelector(".post-canvas-container").parentElement.firstChild.className = 'md:w-1/12 w-full flex flex-col space-y-5 md:mt-0 mt-5 hidden'
 })
