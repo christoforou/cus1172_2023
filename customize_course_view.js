20edbf3f17b5ee523a05e1157d11ecdb54748758
christoforou <42910823+christoforou@users.noreply.github.com>
2025-03-06 06:42:03 -0500

    Update customize_course_view.js
    Added comments.

--- customize_course_view.js
+++ customize_course_view.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {  
     
     editing_mode = document.baseURI.includes("/edit")
-
     
     // Extract information
     data_name = window.parent.document.querySelector("span.hidden").innerHTML
@@ -26,23 +25,25 @@ document.addEventListener('DOMContentLoaded', function() {
     //window.parent.window.document.querySelector("#app").style.backgroundColor = 'blue';     
     //
 
-    // Hide the left sidebar and make the right main content full screen. 
+    // Adjust the style : Hide the left sidebar and make the right main content full screen. 
     window.parent.window.document.querySelector(".post-canvas-container").className = 'w-full md:w-full post-canvas-container';
     //window.parent.window.document.querySelector(".post-canvas-container").parentElement.firstChild.className = 'md:w-1/12 w-full flex flex-col space-y-5 md:mt-0 mt-5'
     window.parent.window.document.querySelector(".post-canvas-container").parentElement.firstChild.className = 'md:w-1/12 w-full flex flex-col space-y-5 md:mt-0 mt-5 hidden'
 
+    
     if (editing_mode) {
         console.log("In Edit mode, do not inject the text")
-    } else {
-        
+    } else {    
             // make the iframe invisible, not occupying space in the window. - might need to change that to some heigh - or position absolute to control view
             // For now, make the component, zero height instead of hidden.
-            //window.parent.window.document.querySelector(".custom-embed-iframe").className = '.custom-embed-iframe hidden'
-            
-            // Need to calculate this based on the content I want to embed - Get this information from an asynch request to the conten generation application. . 
-            window.parent.window.document.querySelector(".custom-embed-iframe").style.height="6600px"
+            // window.parent.window.document.querySelector(".custom-embed-iframe").className = '.custom-embed-iframe hidden'
+
+
         
-            
+        
+            // Need to calculate this based on the content I want to embed - Get this information from an asynch request to the conten generation application. . 
+        
+            window.parent.window.document.querySelector(".custom-embed-iframe").style.height="6600px"
             window.parent.window.document.querySelector(".custom-embed-iframe").id = "rki_custom_course_view"
         
             // Add a new notebook
